d71e924d70d0503cbf45ed507a85a847f8df247f
Navin-8-Anand <161301128+Navin-8-Anand@users.noreply.github.com>
2024-02-26 14:49:52 +0530

    Predicting the Color

---
+++ Color Prediction.py
@@ -0,0 +1,2 @@
+import Utilities as Ult
+csvFileLoc = "E:\Project_works\Dataset\ProjectDataset.csv" modelLocation = 'E:\Project_works\Dataset\ColorPrediction.sav' Ult.saveTrainedModel(csvFileLoc,modelLocation)
