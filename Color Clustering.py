b41a2520ab9496295f66044dea35a6c4b2cdb2c7
Navin-8-Anand <161301128+Navin-8-Anand@users.noreply.github.com>
2024-02-26 14:50:31 +0530

    Clustering the colors

---
+++ Color Clustering.py
@@ -0,0 +1,3 @@
+import Utilities as Ult
+dataPath = "E:\Project_works\Dataset\ProjectDataset.csv" numberOfClusters = 7
+modelLocation = 'E:\Project_works\Dataset\ColorClustering.sav' Ult.saveClusterLabels(dataPath,numberOfClusters,modelLocation)
